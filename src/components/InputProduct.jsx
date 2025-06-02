1260531ef441197e711c50d4080f5513cdff195e
VazquezLD <davlucho86@gmail.com>
2025-06-02 12:32:56 -0300

    fix input style by page

--- src/components/InputProduct.jsx
+++ src/components/InputProduct.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "../styles/Aside.css"
 import { useSearch } from "../context/ContextProvider";
+import { useLocation } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
 export const InputProduct = () => {
 
+    const location = useLocation()
+    const path = location.pathname
+    const inputClass = path === '/products'? "search-bar" : "hidden"
+
     const { search, setSearch } = useSearch()
 
     return(
-        <div className="search-bar">
+        <div className={inputClass}>
             <input type="text" placeholder="Buscar..." value={search} onChange={(e) => setSearch(e.target.value)}/>
             <span className="icon"><FiSearch /></span>
         </div>
